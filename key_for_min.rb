2cdfe6f3dfd8d9a929727c349a49266e85f78f88
Matthew Peters <matthew.peters.nyc@gmail.com>
2020-07-06 18:43:16 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,16 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
-  winner = 1000
-  name_hash.each do |value, key|
-    if key > winner
-      winner = "#{key}"
-
-    end
-  end
-p "#{value}"
- end
+high_val = 1000
+   hash.each do |key, value|
+   if value < high_val
+     high_val = value
+end
 
 
 hash = {:blake => 500, :ashley => 2, :adam => 1}
